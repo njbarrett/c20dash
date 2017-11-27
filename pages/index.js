f8aef27d3f51bf4d70df1410b5c5daae2340fb46
Nick Barrett <njbarrett7@gmail.com>
2017-11-27 20:48:28 +0800

    play with some graphing

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 import fx from 'money'
 import accounting from 'accounting'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 class Index extends React.Component {
 
@@ -50,6 +51,16 @@ class Index extends React.Component {
         fx.rates = this.props.rates
         const usdValue = this.props.c20Data.nav_per_token * this.state.c20Tokens
 
+        const data = [
+              {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+              {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+              {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+              {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+              {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+              {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+              {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+        ];
+
         return (
             <Layout>
             <h1>Crypto20 Dashboard</h1>
@@ -62,6 +73,15 @@ class Index extends React.Component {
                 <li>USD Value of your Tokens: ${accounting.toFixed(usdValue, 2)}</li>
                 <li>AUD Value of your Tokens: ${accounting.toFixed(fx(usdValue).from('USD').to('AUD'), 2)}</li>
             </ul>
+
+            <AreaChart width={600} height={400} data={data}
+                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
+            </AreaChart>
 
             </Layout>
         );
